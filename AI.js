4e1733f8e9493da369f442ad82b9eefc7f402e21
VazquezDev37 <isaacvazquez1119@gmail.com>
2021-09-09 16:18:25 -0500

    updating code for AI.js file

---
+++ AI.js
@@ -0,0 +1,13 @@
+const Player = require("./Player");
+
+class AI extends Player{
+    constructor(){
+        super();
+        this.name = "Computer";
+    }
+
+    selectGesture(){
+        let rand = Math.floor(Math.random()* this.gestures.length);
+        this.chosenGesture = this.gestures[rand];
+    }
+}
